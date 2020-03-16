3b08e30a5d1bb89a7db30a375431554a6b44a305
Joris van de Donk <jorisvddonk@gmail.com>
2020-03-16 14:18:40 +0100

    fix issue where in certain cases the results for one country could be attributed to another

--- src/main.py
+++ src/main.py
@@ -5,13 +5,18 @@ import asyncio
 
 app = FastAPI()
 
+async def getData(coronaut):
+    return {
+        'country': coronaut.get_country_name(),
+        'cases': await coronaut.get_cases()
+    }
 
 @app.get("/")
 async def read_cases():
     cases = {}
     coronautObjects = [coronaut() for [z, coronaut] in inspect.getmembers(src.coronauts, inspect.isclass)]
-    done, pending = await asyncio.wait([c.get_cases() for c in coronautObjects], timeout=3)
-    for i, task in enumerate(done):
-        amount = task.result()
-        cases[coronautObjects[i].get_country_name()] = amount
+    done, pending = await asyncio.wait([getData(c) for c in coronautObjects], timeout=3)
+    for task in done:
+        result = task.result()
+        cases[result['country']] = result['cases']
     return cases
